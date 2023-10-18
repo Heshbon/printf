4681e9661b6c845a17dd7bc61494ac9d28c8ddfe
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-10-17 19:23:06 -0700

    printf

--- printf.c
+++ printf.c
@@ -12,47 +12,81 @@ int _printf(const char *format, ...)
 	va_list n;
 	int length = 0;
 
-	if (!format || (format[0] == '%' && format[1] == '\0') || format == NULL)
+	if (format == NULL)
 	{
 		return (-1);
 	}
 	va_start(n, format);
 	while (*format)
 	{
-		if (*format != '%')
+		if (*format == '%')
+		{
+		
+			format++;
+	       
+		
+			if (*format == 'd' || *format == 'i')
+		
+			{
+			
+				length += printf("d", va_arg(n, int));
+			
+				a++;
+		
+			}
+		
+			else if (*format == 'c')
+		
+			{
+				putchar(va_arg(n, int));
+			
+				length++;
+			
+				a++;
+		
+			}
+		
+			else if (*format  == 's')
+		
+			{
+			
+				s = va_arg(n, char *);
+			
+				while (*s)
+			
+				{
+				
+					putchar(*s);
+				
+					a++;
+				
+					length++;
+			
+				}
+
+		
+			}
+		
+			else if (*format == '%')
+		
+			{
+				putchar('%');
+			
+				length++;
+			
+				a++;
+			}
+		else
 		{
 			putchar(*format);
 			length++;
 		}
 		format++;
-	       
-		if (*format == 'd' || *format == 'i')
-		{
-			length += printf("d", va_arg(n, int));
-			a++;
-		}
-		else if (*format == 'c')
-		{	putchar(va_arg(n, int));
-			length++;
-			a++;
-		}
-		else if (*format  == 's')
-		{
-			s = va_arg(n, char *);
-			while (*s)
-			{
-				putchar(*s);
-				a++;
-				length++;
-			}
-		}
-		else if (*format == '%')
-		{	putchar('%');
-			length++;
-			a++;
-		}
-		format++;
 	}
+	
+	}
+
 	va_end(n);
 	return (length);
+
 }
